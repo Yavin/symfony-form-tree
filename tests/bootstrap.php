22937d51904a5a83cb9a7a0b36f6785ef306ae2d
Jarek W <jarekwierciak@gmail.com>
2013-12-20 22:57:10 +0100

    add travis-ci config file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-define('VENDOR_PATH', realpath(__DIR__ . '/../../..'));
+define('VENDOR_PATH', realpath(__DIR__ . '/../vendor'));
 
 if (!is_file($autoloadFile = VENDOR_PATH.'/autoload.php')) {
     throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
@@ -8,7 +8,7 @@ if (!is_file($autoloadFile = VENDOR_PATH.'/autoload.php')) {
 
 $loader = require $autoloadFile;
 
-//add test namespace
+$loader->add('Yavin\Symfony\Form\Type', __DIR__ . '/../src');
 $loader->add('Yavin\Symfony\Form\Type\Tests', __DIR__);
 
 //register doctrine annotations
